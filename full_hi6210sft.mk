65c22b67c57bd0e4c455ca92bf8d9feb3fd9a68a
hak86 <hakamelassouad@gmail.com>
2016-07-26 20:01:33 +0200

    fix compiling issue

--- full_hi6210sft.mk
+++ full_hi6210sft.mk
@@ -1,3 +1,6 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, device/huawei/hi6210sft/hi6210sft.mk)
+
 PRODUCT_NAME := full_hi6210sft
 PRODUCT_DEVICE := hi6210sft
 PRODUCT_BRAND := hi6210sft
--- hi6210sft.mk
+++ hi6210sft.mk
@@ -7,7 +7,7 @@ PRODUCT_AAPT_CONFIG := xhdpi hdpi normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Blobs
-$(call inherit-product, vendor/hisi/hi6210sft/device-vendor.mk)
+$(call inherit-product, vendor/hisi/hi6210sft/hi6210sft-vendor.mk)
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Chromium 32 Bit
