0fac4dd63e52ce53f7d711095f0f719415c747c4
hak86 <hakamelassouad@gmail.com>
2016-07-26 19:28:05 +0200

    add initial bootable tree

---
+++ hi6210sft.mk
@@ -0,0 +1,80 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := xhdpi hdpi normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Blobs
+$(call inherit-product, vendor/hisi/hi6210sft/device-vendor.mk)
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    hw.lcd.lcd_density=320 \
+    ro.sf.lcd_density=320
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# File System
+PRODUCT_PACKAGES += \
+    	make_ext4fs \
+    	setup_fs
+
+# Graphics
+PRODUCT_PACKAGES += \
+	gralloc.hi6210sft \
+	libGLES_android \
+	libGLES_mali \
+	libion
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    	debug.hwui.render_dirty_regions=false \
+    	persist.sys.strictmode.disable=1 \
+    	persist.sys.use_dithering=2 \
+    	ro.opengles.version=131072
+
+# Kernel
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/huawei/hi6210sft/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    	$(LOCAL_PATH)/rootdir/fstab.hi6210sft:root/fstab.hi6210sft \
+    	$(LOCAL_PATH)/rootdir/init.5801.rc:root/init.5801.rc \
+    	$(LOCAL_PATH)/rootdir/init.audio.rc:root/init.audio.rc \
+    	$(LOCAL_PATH)/rootdir/init.connectivity.hi110x.rc:root/init.connectivity.hi110x.rc \
+    	$(LOCAL_PATH)/rootdir/init.environ.rc:root/init.environ.rc \
+    	$(LOCAL_PATH)/rootdir/init.extmodem.rc:root/init.extmodem.rc \
+    	$(LOCAL_PATH)/rootdir/init.hi6210sft.rc:root/init.hi6210sft.rc \
+    	$(LOCAL_PATH)/rootdir/init.hi6210sft.usb.rc:root/init.hi6210sft.usb.rc \
+    	$(LOCAL_PATH)/rootdir/init.hisi.rc:root/init.hisi.rc \
+    	$(LOCAL_PATH)/rootdir/init.manufacture.rc:root/init.manufacture.rc \
+    	$(LOCAL_PATH)/rootdir/init.platform.rc:root/init.platform.rc \
+    	$(LOCAL_PATH)/rootdir/init.tee.rc:root/init.tee.rc \
+    	$(LOCAL_PATH)/rootdir/init.trace.rc:root/init.trace.rc \
+    	$(LOCAL_PATH)/rootdir/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc
+
+# Zygote
+ADDITIONAL_DEFAULT_PROPERTIES += \
+	ro.zygote=zygote64_32
+
+PRODUCT_COPY_FILES += \
+	system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.zygote=zygote64_32
