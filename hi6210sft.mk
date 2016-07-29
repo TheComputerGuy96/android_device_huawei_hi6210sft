07f3270b8140c7536295c90d60ff11a8d3da8f05
hak86 <hakamelassouad@gmail.com>
2016-07-29 10:48:02 +0200

    revert to initial tree

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -6,34 +6,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 PRODUCT_AAPT_CONFIG := xhdpi hdpi normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-# Bin
-PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/bin/,system/bin) \
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-	bluetooth.default \
-	
-# Boot
-PRE_BOOT_FILES := isp.bin ons.bin phone.prop
-
-PRODUCT_COPY_FILES += \
-       $(LOCAL_PATH)/rootdir/isp.bin:system/isp.bin \
-       $(LOCAL_PATH)/rootdir/ons.bin:system/ons.bin \
-       $(LOCAL_PATH)/rootdir/phone.prop:system/phone.prop
-
-# Camera
-$(call inherit-product, device/huawei/hi6210sft/products/Camera.mk)
-
-PRODUCT_PACKAGES += \
-	camera.hi6210sft
-
-# Chrome
-$(call inherit-product, device/huawei/hi6210sft/products/Chrome.mk)
-
-# Codecs
-$(call inherit-product, device/huawei/hi6210sft/products/Codecs.mk)
-
 # Display
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
@@ -49,9 +21,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 PRODUCT_PACKAGES += \
     	make_ext4fs \
     	setup_fs
-
-# GPS
-$(call inherit-product, device/huawei/hi6210sft/products/GPS.mk)
 
 # Graphics (Don't use hwcomposer.hi6210sft module for now or will not boot)
 PRODUCT_COPY_FILES += \
@@ -86,22 +55,6 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-# Keylayout
-PRODUCT_COPY_FILES += \
-        $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/usr/keylayout,system/usr/keylayout) \
-
-# Fix libWVStreamControlAPI
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/rootdir/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-        $(LOCAL_PATH)/rootdir/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
-
-# LibNVME
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/rootdir/lib64/libnvme.so:system/lib64/libnvme.so \
-
-# Modem
-$(call inherit-product, device/huawei/hi6210sft/products/Modem.mk)
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
@@ -128,56 +81,16 @@ PRODUCT_COPY_FILES += \
        $(LOCAL_PATH)/ramdisk/init.hi6210sft.rc:root/init.hi6210sft.rc \
        $(LOCAL_PATH)/ramdisk/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc
 
-# RIL
-$(call inherit-product, device/huawei/hi6210sft/products/Balong.mk)
-
-PRODUCT_PACKAGES += \
-	hwcustTelephony-common \
-	hwframework \
-	hwServices\
-	hwTelephony-common \
-	hwWifi-services
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	rild.libpath=/system/lib/libbalong-ril.so \
-    	rild.libpath1=/system/lib/libbalong-ril.so \
-	rild.libpath2=/system/lib64/libbalong-ril-1.so \
-
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.dsds_mode=umts_gsm \
 	ro.config.hw_device_mode=clg_mode \
 	ro.multi.rild=false
-
-# Supplicant
-$(call inherit-product, device/huawei/hi6210sft/products/Supl20client.mk)
-
-# Thermald
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/rootdir/etc/thermald.xml:system/etc/thermald.xml \
-        $(LOCAL_PATH)/rootdir/etc/thermald_performance.xml:system/etc/thermald_performance.xml
-
-# TP Huawei
-PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/etc/tp_test_parameters,system/etc/tp_test_parameters) \
 
 # USB
 PRODUCT_PACKAGES += \
 	com.android.future.usb.accessory
 
 # Wifi
-$(call inherit-product, device/huawei/hi6210sft/products/Wifi.mk)
-
-PRODUCT_PACKAGES += \
-	bt_vendor.conf \
-	dhcpcd \
-	hostapd_hisi.conf \
-	oam_app \
-	octty \
-	wpa_cli_hisi \
-	wpa_supplicant_hisi \
- 	wpa_supplicant_hisi.conf \
-    	wpa_supplicant.conf
-
 PRODUCT_PROPERTY_OVERRIDES += \
     	wifi.interface=wlan0 \
     	wifi.supplicant_scan_interval=15
--- products/Modem.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := device/huawei/hi6210sft
-
-# Log
-PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/etc/log,system/etc/log) \
-
-# modemConfig
-PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/etc/modemConfig,system/etc/modemConfig) \
